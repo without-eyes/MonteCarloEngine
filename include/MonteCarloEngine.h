fa50a7fa3f8f9aac51a8d6ee772df45289ab8e9b
without-eyes <dimka180205@gmail.com>
2025-07-27 15:11:50 +0300

    Make Monte Carlo engine multithreaded

--- include/MonteCarloEngine.h
+++ include/MonteCarloEngine.h
@@ -1,23 +1,40 @@
 #ifndef MONTECARLOENGINE_H
 #define MONTECARLOENGINE_H
 
-#include <cstdint>
+#include <numeric>
+#include <thread>
 
 template <typename Experiment>
 class MonteCarloEngine {
 public:
-    explicit MonteCarloEngine(uint64_t trials) : trials(trials) {}
+    explicit MonteCarloEngine(uint64_t trials, double precision) : trials(trials), precision(precision) {}
 
-    double run(Experiment& experiment) {
-        double result = 0;
-        for (uint64_t i = 0; i < trials; i++) {
-            result += experiment.run();
+    double run() {
+        const uint8_t threadCount = std::thread::hardware_concurrency();
+        uint64_t trialsPerThread = trials / threadCount;
+        std::vector<std::thread> threads;
+        std::vector results(threadCount, 0.0);
+
+        for (uint8_t i = 0; i < threadCount; i++) {
+            threads.emplace_back([&, i] {
+                Experiment localExperiment(trialsPerThread, precision);
+                for (uint64_t j = 0; j < trialsPerThread; j++) {
+                    results[i] += localExperiment.run();
+                }
+            });
         }
+
+        for (auto& thread : threads) {
+            thread.join();
+        }
+
+        double result = std::accumulate(results.begin(), results.end(), 0.0);
         return result / trials;
     }
 
 private:
     uint64_t trials;
+    double precision;
 };
 
 #endif //MONTECARLOENGINE_H
--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,11 @@
 #include "../include/PiCalculation.h"
 
 int main() {
-    auto piCalculation = PiCalculation(10000, 0.01);
-    auto engine = MonteCarloEngine<PiCalculation>(10000);
+    auto engine = MonteCarloEngine<PiCalculation>(10000, 0.001);
 
     std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 
-    double result = engine.run(piCalculation);
+    double result = engine.run();
 
     std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
